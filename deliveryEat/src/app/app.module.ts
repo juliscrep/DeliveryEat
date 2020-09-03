36ae5484f274517d225ba9d005d792d8510f987b
julieta <julietascrepnik@gmail.com>
2020-09-03 20:35:30 -0300

    cambios en rutas

--- deliveryEat/src/app/app.module.ts
+++ deliveryEat/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {PedidoComponentComponent} from './components/pedido-component/pedido-component.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,8 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule //, RouterModule.forRoot([
-     // { path: 'Pedidos', component: PedidoComponentComponent}    ])
+    AppRoutingModule , RouterModule.forRoot([
+     { path: 'Pedidos', component: PedidoComponentComponent}    ])
   ],
   providers: [],
   bootstrap: [AppComponent,PedidoComponentComponent]
