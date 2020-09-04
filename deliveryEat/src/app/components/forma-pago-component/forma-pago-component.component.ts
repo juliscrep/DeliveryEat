a515c9b063836dfa27b5ac5a4d756f0cc5c9f673
julieta <julietascrepnik@gmail.com>
2020-09-04 13:03:00 -0300

    cambios en feedback

--- deliveryEat/src/app/components/forma-pago-component/forma-pago-component.component.ts
+++ deliveryEat/src/app/components/forma-pago-component/forma-pago-component.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forma-pago-component.component.css']
 })
 export class FormaPagoComponentComponent implements OnInit {
-
+  mostrarSelec=true
   constructor() { }
 
   ngOnInit(): void {
