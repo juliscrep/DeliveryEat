0e42ba0c1ee8a2c66ab947f6ce31a30f29c24756
julieta <julietascrepnik@gmail.com>
2020-09-08 11:37:19 -0300

    resumen

--- deliveryEat/src/app/components/steps-pedido-lo-que-sea/resumen-pedido/resumen-pedido.component.ts
+++ deliveryEat/src/app/components/steps-pedido-lo-que-sea/resumen-pedido/resumen-pedido.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 import { Pedido } from 'src/app/domain/interfaces/pedido.interface';
 
 @Component({
